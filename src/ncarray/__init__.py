e499a5db71015636d0c6d9e1c430d27b236f067d
gadorlhiac <gadorlhiac@gmail.com>
2026-05-26 18:54:12 -0700

    MNT Update utility functions get_include and get_lib_dir for the conda install paths. Wheel install paths should still work.

--- src/ncarray/__init__.py
+++ src/ncarray/__init__.py
@@ -6,19 +6,44 @@
 
 import contextlib
 import os
+import sys
 
 from ncarray.core._pyncarray import *
+
 try:
     from ncarray.core._pyncdevarray import *
 except (ModuleNotFoundError, ImportError):
     ...
 
+
 def get_include() -> str:
-    return os.path.join(os.path.dirname(__file__), "include")
+    nca_wheel_include_dir: str = os.path.join(os.path.dirname(__file__), "include")
+    if os.path.exists(nca_wheel_include_dir):
+        return nca_wheel_include_dir
+
+    # When installing with conda or not pure pip, the headers will be in the standard
+    # prefix
+    win_prefix_include_dir: str = os.path.join(sys.prefix, "Library", "include")
+    if os.path.exists(win_prefix_include_dir):
+        return win_prefix_include_dir
+
+    unix_prefix_include_dir: str = os.path.join(sys.prefix, "include")
+    return unix_prefix_include_dir
 
 
 def get_lib_dir() -> str:
-    return os.path.join(os.path.dirname(__file__), "lib")
+    nca_wheel_lib_dir: str = os.path.join(os.path.dirname(__file__), "lib")
+    if os.path.exists(nca_wheel_lib_dir):
+        return nca_wheel_lib_dir
+
+    # When installing with conda or not pure pip, the lib will be in the standard
+    # prefix
+    win_prefix_lib_dir: str = os.path.join(sys.prefix, "Library", "lib")
+    if os.path.exists(win_prefix_lib_dir):
+        return win_prefix_lib_dir
+
+    unix_prefix_lib_dir: str = os.path.join(sys.prefix, "lib")
+    return unix_prefix_lib_dir
 
 
 @contextlib.contextmanager
